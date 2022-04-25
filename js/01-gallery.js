c0e3c3462cd0219203711aa17ee8f4f901ec0ace
Yuliia <littlehappinies@gmail.com>
2022-04-25 21:47:44 +0300

    Add gallery markup

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,31 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryItemsContainer = document.querySelector('.gallery');
+const galleryItemsMarkup = createGalleryItemsMarkup(galleryItems);
+
+galleryItemsContainer.insertAdjacentHTML('beforeend', galleryItemsMarkup);
+
+function createGalleryItemsMarkup(galleryItems) {
+    return galleryItems.map(({ original, description, preview }) => {
+        return ` <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+    `;
+    }).join('');
+};
+
+galleryItemsContainer.addEventListener('click', (event) => {
+    event.preventDefault();
+});
+
+
+
